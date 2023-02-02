0f5bb356b6036aba1bddb669369749c5f56c9cd9
batorlancs <batorg@lancaster.ac.uk>
2023-02-02 18:58:18 +0000

    edited header animations, articles section redone, added animation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,43 +1,61 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import Linkedin from '../../pic/sociallogos/linkedin.png';
 import Github from '../../pic/sociallogos/github.png';
 import Facebook from '../../pic/sociallogos/facebook.png';
 
+const initState = { left: "ml-[10%]", right: "mr-[10%]"};
+
+const enum REDUCER_ACTION_TYPE {
+    ANIMATION_IN,
+    ANIMATION_OUT
+}
+
+type ReducerAction = {
+    type: REDUCER_ACTION_TYPE;
+}
+
+const reducer = (state: typeof initState, action: ReducerAction): typeof initState => {
+    switch(action.type) {
+        case REDUCER_ACTION_TYPE.ANIMATION_IN:
+            return {...state, left: "ml-[10%]", right:"mr-[10%]"}
+        case REDUCER_ACTION_TYPE.ANIMATION_OUT:
+            return {...state, left: "opacity-0 ml-0", right:"opacity-0 mr-0"}
+        default:
+            throw new Error();
+    }
+}
+
 function Header() {
 
-    const [anim, setAnim] = useState('');
-    const [anim2, setAnim2] = useState('');
+    const [state, dispatch] = useReducer(reducer, initState);
+    const buttonStyle = 'text-black text-2xl tracking-wider';
 
     window.addEventListener(('scroll'), () => {
         if (window.scrollY === 0) {
-            setAnim('');
-            setAnim2('');
+            dispatch({ type: REDUCER_ACTION_TYPE.ANIMATION_IN});
         } else {
-            setAnim('ml-[0%] opacity-0');
-            setAnim2('mr-[0%] opacity-0');
+            dispatch({ type: REDUCER_ACTION_TYPE.ANIMATION_OUT});
+
         }
     });
 
-    const buttonStyle = 'text-black text-2xl tracking-wider';
-
     return (
       <div className='absolute h-40 w-full flex flex-row items-center justify-between top-0 left-0 z-50 font-body'>
-            <div className={`py-20 text-4xl ml-[10%] font-bold text-black duration-500 ${anim}`}>
+            <div className={`py-20 text-4xl font-bold text-black duration-500 ${state.left}`}>
                 Terminal.
             </div>
-            <div className={`flex flex-row items-center justify-end gap-16 w-[25%] mr-[10%] h-full duration-500 ${anim2}`}>
+            <div className={`flex flex-row items-center justify-end gap-16 w-[25%] h-full duration-500 ${state.right}`}>
                 <button className={buttonStyle}>projects</button>
                 <button className={buttonStyle}>skills</button>
                 <button className={buttonStyle}>experience</button>
-                {/* <button className={buttonStyle}>contact</button> */}
                 <div className='flex flex-row gap-5'>
-                    <button className='h-14 w-14 rounded-full overflow-hidden duration-500 bg-white hover:bg-blue-400'>
+                    <button className='h-14 w-14 rounded-full overflow-hidden duration-500 bg-opacity-0'>
                         <img src={Linkedin}></img>
                     </button>
-                    <button className='h-14 w-14 rounded-full overflow-hidden duration-500 bg-white hover:bg-blue-400'>
+                    <button className='h-14 w-14 rounded-full overflow-hidden duration-500 bg-opacity-0'>
                         <img src={Github}></img>
                     </button>
-                    <button className='h-14 w-14 rounded-full overflow-hidden duration-500 bg-white hover:bg-blue-400'>
+                    <button className='h-14 w-14 rounded-full overflow-hidden duration-500 bg-opacity-0'>
                         <img src={Facebook}></img>
                     </button>
                 </div>
--- src/components/homepage/hero/articles/Articles.tsx
+++ src/components/homepage/hero/articles/Articles.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
 import ArrowRight from '../../../../pic/arrow-right.svg';
 import './Articles.css';
+import 'aos/dist/aos.css';
 
 function Articles() {
+
+    const cards = [{
+        title: "Article Title of Something",
+        desc: "This is a description about the article, a little intro about what it looks like",
+        time: "2 hours ago",
+        badgeColor: "bg-blue-500",
+        order: 0
+    }, {
+        title: "Article Title of Another Thing",
+        desc: "This is a description about the article, a little intro about what it looks like",
+        time: "4 days ago",
+        badgeColor: "bg-orange-500",
+        order: 1
+    },
+    ]
+
     return (
         <div className='pt-56'>
             <div className='flex flex-row justify-between items-center gap-20 min-h-72'>
+
                 <div className='min-w-[500px] h-full flex flex-col justify-center items-start z-20'>
                     <h2 className='text-4xl font-bold tracking-tight'>My Coding Journey</h2>
                     <p className='text-2xl mt-5'>Take a look at my recent articles about my progress and plans for the future.</p>
-                    <button className='articlebutton mt-10 h-14 text-2xl opacity-50 tracking-wider flex flex-row justify-start items-center'>
+                    <button className='articlebutton mt-10 h-14 text-2xl opacity-50 tracking-wider flex flex-row justify-start items-center hover:opacity-100 duration-500'>
                         <img src={ArrowRight} className='h-8 duration-500'></img>
-                        <p className='duration-500'>See all articles</p>
+                        <p>See all articles</p>
                     </button>
                 </div>
 
-                <div className='w-full h-full p-6 bg-white bg-opacity-30 border-white border-2 rounded-3xl shadow-2xl z-20'>
-                    <div className='flex flex-row items-center justify-between w-full'>
-                        <img className='h-3 w-12 bg-orange-500 bg-opacity-50 rounded-full'></img>
-                        <p className='text-lg font-semibold opacity-50'>2 Hours Ago</p>
+                {cards.map((card) => (
+                    <div className='w-full h-full p-6 bg-white bg-opacity-30 border-white border-2 rounded-3xl shadow-2xl z-20'
+                    data-aos='fade-left' data-aos-duration='1000' data-aos-delay={card.order * 200}>
+                        <div className='flex flex-row items-center justify-between w-full'>
+                            <img className={`h-5 w-20 bg-opacity-50 rounded-full ${card.badgeColor}`}></img>
+                            <p className='text-lg font-semibold opacity-50'>{card.time}</p>
+                        </div>
+                        <h3 className='mt-5 text-2xl font-bold'>{card.title}</h3>
+                        <p className='mt-2 pb-10 text-xl'>{card.desc}</p>
                     </div>
-                    <h3 className='mt-5 text-2xl font-bold'>Article Title of Something</h3>
-                    <p className='mt-5 text-xl'>This is a description about the article, a little intro about what it looks like</p>
-                </div>
-
-                <div className='w-full h-full p-6 bg-white bg-opacity-30 border-white border-2 rounded-3xl shadow-2xl z-20'>
-                    <div className='flex flex-row items-center justify-between w-full'>
-                        <img className='h-3 w-12 bg-blue-500 bg-opacity-50 rounded-full'></img>
-                        <p className='text-lg font-semibold opacity-50'>2 Hours Ago</p>
-                    </div>
-                    <h3 className='mt-5 text-2xl font-bold'>Article Title of Something</h3>
-                    <p className='mt-5 text-xl'>This is a description about the article, a little intro about what it looks like</p>
-                </div>
+                ))}
+                
             </div>
         </div>
     )
