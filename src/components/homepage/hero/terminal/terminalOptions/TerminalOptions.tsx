5afae7b89795ac79e13d987991a44126d84dca47
batorlancs <batorg@lancaster.ac.uk>
2023-03-28 21:59:43 +0100

    terminal design rework, projects section responsivity added

--- src/components/homepage/hero/terminal/terminalOptions/TerminalOptions.tsx
+++ src/components/homepage/hero/terminal/terminalOptions/TerminalOptions.tsx
@@ -1,28 +1,51 @@
 import React from "react";
-import ArrowRight from "../../../../../pic/arrow-right.svg";
 
 
 const TerminalOptions = () => {
 
-	const terminalWelcome = [
-		"hi!",
-		"I am using 'OpenAI' API to answer your questions.",
-		"Ask me anything that you want to know about me!",
-	];
+    const terminalMessages = [
+        {
+            highlighted: true,
+            message: "hi!"
+        },
+        {
+            highlighted: false,
+            message: "Here you can ask anything that you want to know about me!"
+        },
+        {
+            highlighted: false,
+            message: "-"
+        },
+        {
+            highlighted: true,
+            message: "you can also try:"
+        }
+        
+    ]
+
+    const terminalOptions = [
+        "> tell me about your projects",
+        "> do you have relevant tech experience",
+        "> do you have pets?"
+    ]
 
 	return (
 		<div className="max-xl:hidden">
-			{terminalWelcome.map((line) => (
-				<div
-					className="flex flex-row items-start justify-start"
-					key={line}
-				>
-					<img src={ArrowRight} className="h-8 invert" alt="arrow-right-icon"></img>
-					<p className="text-s border-none bg-transparent pt-[4px] text-white">
-						{line}
-					</p>
-				</div>
-			))}
+            <div className='flex flex-row justify-start items-start max-xl:hidden py-2'>
+                <p className='text-xl pr-1 pt-[3px]'>👨🏼‍💼</p>
+                <p className='pt-[4px] bg-transparent border-none text-neutral-400 text-s w-full'>
+                    {terminalMessages.map((terminalMessage) => (
+                        <p className={terminalMessage.highlighted ? "text-emerald-500" : ""}>
+                            {terminalMessage.message}
+                        </p>
+                    ))}
+                    {terminalOptions.map((terminalOption) => (
+                        <p className=" text-red-400 opacity-50">
+                            {terminalOption}
+                        </p>
+                    ))}
+                </p>
+            </div>
 			<hr className="my-3 ml-3 w-full border-t-2 border-dashed opacity-30" />
 		</div>
 	);
